d24cf0605dbbd9e299bbeac2400c88ddecabbfff
Alex <older.miller.ua@gmail.com>
2022-04-05 11:59:32 +0200

    Pages added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './components/app/app';
+import OurCoffee from './components/ourCoffee/ourCoffee';
+import ForPleasure from './components/forPleasure/forPleasure';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>} />
+      <Route path="/ourcoffee" element={<OurCoffee/>} />
+      <Route path="/forpleasure" element={<ForPleasure/>} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
