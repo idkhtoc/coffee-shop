909b9691fafe178e2267cf612207897a823867c0
Alex <older.miller.ua@gmail.com>
2022-04-05 11:13:21 +0200

    Init commit

---
+++ src/components/app/app.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="app">
+                
+            </div>
+        );
+    }
+}
+
+export default App;
